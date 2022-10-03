7ffb314b3416ed250ed960f97856f1bad79849f7
KunjYadav <kunj.yadav555@gmail.com>
2022-10-04 01:15:03 +0530

    Task -14 -> Adding Pagination

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cart_items = document.querySelector('#cart .cart-items');
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('data');
+    displayProducts();
 
     axios.get('http://localhost:3000/products').then((data) => {
         console.log(data);
-        if(data.request.status === 200) {
+        if (data.request.status === 200) {
             const products = data.data.products;
             const parentSection = document.getElementById('Products');
             products.forEach(product => {
@@ -45,9 +46,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 function addToCart(productId) {
-    axios.post('http://localhost:3000/cart', {productId: productId})
-        .then(response =>{
-            if(response.status === 200) {
+    axios.post('http://localhost:3000/cart', { productId: productId })
+        .then(response => {
+            if (response.status === 200) {
                 notifyUsers(response.data.message);
             } else {
                 throw new Error(response.data.message);
@@ -63,10 +64,10 @@ function addToCart(productId) {
 function getCartDetails() {
     axios.get('http://localhost:3000/cart')
         .then(response => {
-            if(response.status === 200) {
+            if (response.status === 200) {
                 response.data.products.forEach(product => {
                     const cartContainer = document.getElementById('cart');
-                //    cartContainer.innerHTML += `<li>${product.title} - ${product.price} - ${product.cartItem.quantity}</li>`
+                    //    cartContainer.innerHTML += `<li>${product.title} - ${product.price} - ${product.cartItem.quantity}</li>`
                 })
                 document.querySelector('#cart').style = "display:block;"
             } else {
@@ -86,58 +87,58 @@ function notifyUsers(message) {
     notification.classList.add('notification');
     notification.innerHTML = `<h4>${message}<h4>`;
     container.appendChild(notification);
-    setTimeout(()=>{
+    setTimeout(() => {
         notification.remove();
-    },2500)
+    }, 2500)
 }
 
 
-document.addEventListener('click',(e)=>{
+document.addEventListener('click', (e) => {
 
-    if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
+    if (e.target.className == 'cart-btn-bottom' || e.target.className == 'cart-bottom' || e.target.className == 'cart-holder') {
         // const cartContainer = document.getElementById('cart');
         // cartContainer.innerHTML = ''
         getCartDetails();
         displayInCart();
-        
+
     }
-    
-    if (e.target.className=='cancel'){
+
+    if (e.target.className == 'cancel') {
         document.querySelector('#cart').style = "display:none;"
     }
 
-    if (e.target.className=='purchase-btn'){
-        if (parseInt(document.querySelector('.cart-number').innerText) === 0){
+    if (e.target.className == 'purchase-btn') {
+        if (parseInt(document.querySelector('.cart-number').innerText) === 0) {
             alert('You have Nothing in Cart , Add some products to purchase !');
             return
         }
         alert('This Feature is yet to be completed ')
     }
 
-    if (e.target.innerText=='REMOVE'){
-        axios.post('http://localhost:3000/cart-delete-item',{'productId':event.target.parentNode.parentNode.id.substring(8)})
-        .then(response=>{
-        removeFromFrontendCart(e)
-        notifyUser(response.data.message);  
-        })
-        .catch(errmsg=>{
-        console.log(errmsg)
-        })
+    if (e.target.innerText == 'REMOVE') {
+        axios.post('http://localhost:3000/cart-delete-item', { 'productId': event.target.parentNode.parentNode.id.substring(8) })
+            .then(response => {
+                removeFromFrontendCart(e)
+                notifyUser(response.data.message);
+            })
+            .catch(errmsg => {
+                console.log(errmsg)
+            })
 
     }
 })
 
-function showProductsInCart(listofproducts){
+function showProductsInCart(listofproducts) {
     cart_items.innerHTML = "";
     listofproducts.forEach(product => {
         const id = `album-${product.id}`;
         const name = document.querySelector(`#${id} h3`).innerText;
         const img_src = document.querySelector(`#${id} img`).src;
         const price = product.price;
-        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
+        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) + 1
         const cart_item = document.createElement('div');
         cart_item.classList.add('cart-row');
-        cart_item.setAttribute('id',`in-cart-${id}`);
+        cart_item.setAttribute('id', `in-cart-${id}`);
         cart_item.innerHTML = `
         <span class='cart-item cart-column'>
         <img class='cart-img' src="${img_src}" alt="">
@@ -151,9 +152,9 @@ function showProductsInCart(listofproducts){
         cart_items.appendChild(cart_item)
     })
 }
-function deleteCartItem(e, prodId){
+function deleteCartItem(e, prodId) {
     e.preventDefault();
-    axios.post('http://localhost:3000/cart-delete-item', {productId: prodId})
+    axios.post('http://localhost:3000/cart-delete-item', { productId: prodId })
         .then(() => removeElementFromCartDom(prodId))
 }
 
@@ -163,27 +164,90 @@ function deleteCartItem(e, prodId){
 //         showNotification('Succesfuly removed product')
 // }
 
-function removeFromFrontendCart(event){
+function removeFromFrontendCart(event) {
     let total_cart_price = document.querySelector('#total-value').innerText;
-    let item_price=document.querySelector(`#${event.target.parentNode.parentNode.id} .cart-price`).innerText;
-    let item_quantity=event.target.parentNode.firstElementChild.value;
+    let item_price = document.querySelector(`#${event.target.parentNode.parentNode.id} .cart-price`).innerText;
+    let item_quantity = event.target.parentNode.firstElementChild.value;
 
-    total_cart_price = parseFloat(total_cart_price).toFixed(2) - (parseFloat(item_price).toFixed(2) * parseInt(item_quantity)) ;
-    document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)-1
+    total_cart_price = parseFloat(total_cart_price).toFixed(2) - (parseFloat(item_price).toFixed(2) * parseInt(item_quantity));
+    document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText) - 1
     document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
     event.target.parentNode.parentNode.remove()
 }
 
-function displayInCart(queryParams=''){
+function displayProducts(queryParams=''){
+    axios.get(`http://localhost:3000/products/${queryParams}`)
+    .then(result=>{
+        const productsDisplayContainerDiv=document.getElementById('Products')
+        productsDisplayContainerDiv.innerHTML='';
+        result.data.products.forEach(product=>{
+            const eachProduct=document.createElement('div');
+            eachProduct.setAttribute('id',`p${product.id}`)
+            eachProduct.innerHTML=`<h3>${product.title}</h3> 
+            <div> <img src="${product.imageUrl}" alt="Image not Found"> </div>  
+            <div> <p>${product.description}</p></div> 
+            <div> 
+                <span>$</span> 
+                <span>${product.price} </span> 
+                <span> <button onClick="addToCart(${product.id})"> Add To Cart </button> </span> 
+            </div> 
+            <br>`
+            productsDisplayContainerDiv.appendChild(eachProduct)
+        })
+        
+        pagination(result,document.getElementById('productPagination'),'Products');
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+}
+
+
+//pagination
+function pagination(response,container,place){
+    let func;
+    if(place=='cart')
+    func='displayInCart'
+    else if(place=='Products')
+    func='displayProducts'
+
+    container.innerHTML=`
+    <span>
+        <button id="${place}firstPage" onclick="${func}('?page=1')">1</button>
+        <button id="${place}previousPage" onclick="${func}('?page=${response.data.previousPage}')">${response.data.previousPage}</button>
+        <button id="${place}currentPage" onclick="${func}('?page=${response.data.currentPage}')" class="active">${response.data.currentPage}</button>
+        <button id="${place}nextPage" onclick="${func}('?page=${response.data.nextPage}')">${response.data.nextPage}</button>
+        <button id="${place}lastPage" onclick="${func}('?page=${response.data.lastPage}')">${response.data.lastPage}</button>
+    </span>
+    `
+    const firstPage=document.getElementById(`${place}firstPage`);
+    const currentPage=document.getElementById(`${place}currentPage`);
+    const previousPage=document.getElementById(`${place}previousPage`);
+    const nextPage=document.getElementById(`${place}nextPage`);
+    const lastPage=document.getElementById(`${place}lastPage`);
+    if(parseInt(currentPage.innerText)==1)
+    firstPage.style='display:none'
+    if(parseInt(previousPage.innerText)<1 || parseInt(previousPage.innerText)==firstPage.innerText)
+    previousPage.style='display:none'
+    if(parseInt(nextPage.innerText)>parseInt(lastPage.innerText))
+    nextPage.style='display:none'
+    if(parseInt(currentPage.innerText)==parseInt(lastPage.innerText) || parseInt(nextPage.innerText)==parseInt(lastPage.innerText) )
+    lastPage.style='display:none'
+
+
+}
+
+function displayInCart(queryParams = '') {
     axios.get(`http://localhost:3000/cart/${queryParams}`)
-    .then(response=>{
-        const cart_items = document.querySelector('#cart .cart-items');
-        cart_items.innerHTML='';
-        response.data.products.forEach((prod)=>{
-        const cart_item = document.createElement('div');
-        cart_item.classList.add('cart-row');
-        cart_item.setAttribute('id',`in-cart-${prod.id}`)
-        cart_item.innerHTML = `
+        .then(response => {
+            const cart_items = document.querySelector('#cart .cart-items');
+            cart_items.innerHTML = '';
+            response.data.products.forEach((prod) => {
+                const cart_item = document.createElement('div');
+                cart_item.classList.add('cart-row');
+                cart_item.setAttribute('id', `in-cart-${prod.id}`)
+                cart_item.innerHTML = `
         <span class='cart-item cart-column'>
         <img class='cart-img' src="${prod.imageUrl}" alt="">
             <span>${prod.title}</span>
@@ -193,27 +257,27 @@ function displayInCart(queryParams=''){
         <input type="text" value="${prod.cartItem.quantity}">
         <button>REMOVE</button>
         </span>`
-        cart_items.appendChild(cart_item)
+                cart_items.appendChild(cart_item)
+
+            })
+            //for amount and no. of products in cart
+            let totalAmount = 0;
+            let totalproducts = 0;
+
+            response.data.allProducts.forEach((prod) => {
+                totalAmount += (parseFloat(prod.price) * parseFloat(prod.cartItem.quantity))
+                totalproducts++;
+            })
+
+            pagination(response, document.getElementById('cartPagination'), 'cart');
+            document.querySelector('#cart').style = "display:block"
+            document.querySelector('#total-value').innerText = totalAmount.toFixed(2);
+            document.querySelector('.cart-number').innerText = totalproducts;
+
 
         })
-        //for amount and no. of products in cart
-        let totalAmount=0;
-        let totalproducts=0;
-
-        response.data.allProducts.forEach((prod)=>{
-            totalAmount+=(parseFloat(prod.price)*parseFloat(prod.cartItem.quantity))
-            totalproducts++;
+        .catch(err => {
+            console.log(err)
         })
 
-        pagination(response,document.getElementById('cartPagination'),'cart');
-        document.querySelector('#cart').style = "display:block"
-        document.querySelector('#total-value').innerText=totalAmount.toFixed(2);
-        document.querySelector('.cart-number').innerText=totalproducts;
-
-
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-    
 }  
